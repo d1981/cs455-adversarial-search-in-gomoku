b429110ac7f93754b9c4511353337e721aa04084
Josh Kapple <joshkapple@gmail.com>
2017-02-08 21:57:34 -0800

    - Added method to get the information provided by gomoku and turn it into an arraylist with a 2d char array for the grid and strings for the pieces of information provided.

--- src/GomokuDriver.java
+++ src/GomokuDriver.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public class GomokuDriver {
    private static final int RACKETPORT = 17033;         // uses port 1237 on localhost  
+   private static final int GRIDWIDTH  = 11;            // Width of the Gomoku Board
+   private static final int GRIDHEIGHT = 11;            // Height of the Gomoku Board
    protected RacketClient rc;
-   
+      
    public GomokuDriver(String h, int p){
       rc = new RacketClient(h, p);
    }
@@ -11,18 +15,69 @@ public class GomokuDriver {
    	client.run();
    }
    
-   public void think(){
-      try {
-         String status = rc.gridIn.readLine().toLowerCase();
-         System.out.println(status);
-         }
-      catch(Exception e) { e.printStackTrace(); }
+   /**
+   * Parses the strings from the server into 
+   * 2d char array for analyzing, String for gamestatus and String for player identification
+   * 
+   * Returns an arraylist of generic objects of the above 3 items
+   */   
+   public ArrayList<Object> getStatus(){
+      // Todo: on first run through, determine the size of the 2d array that represents the Gomoku board, instead of relying on constants      
+   
+      char[][] gridArray = new char[GRIDHEIGHT][GRIDWIDTH];
+      String gameStatus = "";
+      String player = "";
+      
+      ArrayList objectList = new ArrayList<Object>();
 
+      try {
+         for (int i=0; i<(GRIDHEIGHT + 2); i++){
+               if (i == 0){ 
+                   gameStatus = rc.gridIn.readLine().toLowerCase();
+               }
+               else if (i < GRIDHEIGHT + 1){
+                   
+                   String boardRow = rc.gridIn.readLine().toLowerCase();
+                   for (int j=0; j<boardRow.length(); j++){
+                      gridArray[i-1][j] = boardRow.charAt(j); // gridArray[i-1] because the grid lines are actually coming from the second rc.gridLn.readLine(); call                   }
+               }
+               else {
+                   player = rc.gridIn.readLine().toLowerCase();
+               }
+            }
+       }
+      catch(Exception e){
+         e.printStackTrace();
+      }
+         objectList.add(gridArray);
+         objectList.add(gameStatus);
+         objectList.add(player);
+      
+      return objectList;
+   }
+   
+   public void think(){
+      ArrayList  status;
+
+      // Start timer
+      
+      
+      status = getStatus();
+
+      // Analyze status for features
+      
+
+      // Propogate moves / use Alpha Beta to determine best move before timer runs out
+      
+      
+      // Send move
+      
+            
    }
    
    public void run() {
-    	       
-          think();     
-      
+    	    while(true){
+             think();    
+          }
     }
 }
