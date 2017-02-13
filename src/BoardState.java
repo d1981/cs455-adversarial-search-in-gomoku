d11b0f879791fc15bec4ef29fce1e95b08246e30
MarkAHuff <huffm@sou.edu>
2017-02-12 20:28:12 -0800

    Edited BoardState.java
    Added the vertical, horizontal forward/backwards checks to checkOpenness function. 
    
    However, not quite sure how to implement into evaluate.

--- src/BoardState.java
+++ src/BoardState.java
@@ -15,7 +15,6 @@ class BoardState{
       score = 0;
    }
    
-     
    public BoardState(int myscore){
       grid = null;
       firstMove = new int[2];
@@ -53,24 +52,41 @@ class BoardState{
    public int checkOpenness(int row, int column, int direction, int length){
        int openness = 0;
        
+       //System.out.println("Row: " + row + " Column " + column + " Direction " + direction + " Length " + length);
+       
        if (direction == 0){ //Check openness in a horizontal pattern
            // check left hand side
            try {
-              if (grid[row][column-(length + 1)] == ' '){openness+=1;}
-              else if (grid[row][column-(length + 1)] != grid[row][column]){openness+=0;}
-              else {openness+=1;};
+              if (grid[row][column-(length + 1)] == ' '){
+              openness+=1;
+              //System.out.println("In the if.");
+              }
+              else if (grid[row][column-(length + 1)] != grid[row][column]){
+              openness+=0;
+              //System.out.println("In the else if.");
+              }
+              else {
+              openness+=1;
+              //System.out.println("In the else.");
+              }
            } catch (IndexOutOfBoundsException e){
-              //System.err.println("Index error: " + e.getMessage());
+              System.err.println("Left hand side index error: " + e.getMessage());
            }
                
            // check right hand side 
            try {
-              if (grid[row][column] == ' '){openness+=1;}
-              else if (grid[row][column-length] != grid[row][column]){openness+=0;}
-              else {openness+=1;};
+              if (grid[row][column] == ' '){
+              openness+=1;
+              }
+              else if (grid[row][column-length] != grid[row][column]){
+              openness+=0;
+              }
+              else {
+              openness+=1;
+              }
               
            } catch (IndexOutOfBoundsException e){
-              //System.err.println("Index error: " + e.getMessage());
+              System.err.println("Right hand side index error: " + e.getMessage());
            }
        }    
        return openness;
@@ -97,12 +113,17 @@ class BoardState{
       
       // Evaluate the horizontal rows for 2,3,4 or 5 in a row matches
       for (int i=0; i < grid.length; i++){
+         System.out.println("i: " + i);
+      
          for (int j=0; j < grid[i].length; j++){
+            System.out.println("j: " + j);
+            
             if (grid[i][j] == myplayer){
                
                inarowcount++;
                
-               if(opponentinarowcount > 0){ 
+               if(opponentinarowcount > 0){
+                
                   int openness = checkOpenness(i,j,0,opponentinarowcount);
                                
                   switch(opponentinarowcount){
@@ -120,7 +141,7 @@ class BoardState{
                   }
                   opponentinarowcount=0;
                }
-            } // end myplayer check
+            }
             
             else if(grid[i][j] == myopponent){
                opponentinarowcount++;
@@ -142,7 +163,7 @@ class BoardState{
                   }
                   inarowcount=0;  
                }         
-            } // end myopponent check
+            }
             
             else if(grid[i][j] == ' '){
                if (inarowcount > 0){
@@ -161,7 +182,7 @@ class BoardState{
                      case 5: score = 1000000; break;
                   }
                   inarowcount=0;  
-               } 
+               }
                               
                if(opponentinarowcount > 0){ 
                   int openness = checkOpenness(i,j,0,opponentinarowcount);
@@ -173,7 +194,7 @@ class BoardState{
                              
                      case 3: if (openness == 1){opponentthreeinrow_open1++;}
                              else if (openness == 2){opponentthreeinrow_open2++;}
-                            break;
+                             break;
                      case 4: if (openness == 1){opponentfourinrow_open1++;}
                              else if (openness == 2){opponentfourinrow_open2++;}
                              break;
@@ -181,47 +202,10 @@ class BoardState{
                   }
                   opponentinarowcount=0;
                }               
-               inarowcount=0;  
-            } // end white space check
-   
-         }// Row changed, check both again
-
-            if(opponentinarowcount > 0){ 
-                     int openness = checkOpenness(i,grid[i].length,0,opponentinarowcount);
-                                  
-                     switch(opponentinarowcount){
-                        case 2: if (openness == 1){opponenttwoinrow_open1++;}
-                                else if (openness == 2){opponenttwoinrow_open2++;}
-                                break;
-                                
-                        case 3: if (openness == 1){opponentthreeinrow_open1++;}
-                                else if (openness == 2){opponentthreeinrow_open2++;}
-                                break;
-                        case 4: if (openness == 1){opponentfourinrow_open1++;}
-                                else if (openness == 2){opponentfourinrow_open2++;}
-                                break;
-                        case 5: score = -1000000; break;
-                     }
-                     opponentinarowcount=0;
+               inarowcount=0;
+               
             }
-                  
-            if (inarowcount > 0){
-                     int openness = checkOpenness(i,grid[i].length,0,opponentinarowcount);
-                     
-                     switch(inarowcount){
-                        case 2: if (openness == 1){twoinrow_open1++;}
-                                else if (openness==2){twoinrow_open2++;}
-                                break;
-                        case 3: if (openness == 1){threeinrow_open1++;}
-                                else if (openness==2){threeinrow_open2++;}
-                                break;
-                        case 4: if (openness == 1){fourinrow_open1++;}
-                                else if (openness==2){fourinrow_open2++;}
-                                break;
-                        case 5: score = 1000000; break;
-                     }
-                     inarowcount=0;  
-           }
+         }
       }
 
       score += twoinrow_open1*1 + threeinrow_open1*2 + fourinrow_open1*4;
